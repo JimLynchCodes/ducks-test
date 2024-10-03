80fc0352bd0e24d73136447ca97e72a7dfcb6065
JimLynchCodes <mrdotjim@gmail.com>
2024-10-03 12:39:39 -0400

    joystick controlling ball but not duck

--- src/demo/player.rs
+++ src/demo/player.rs
@@ -7,6 +7,9 @@ use bevy::{
     prelude::*,
     render::texture::{ImageLoaderSettings, ImageSampler},
 };
+use virtual_joystick::{
+    create_joystick, JoystickFloating, JoystickInvisible, NoAction, VirtualJoystickEvent, VirtualJoystickPlugin
+};
 
 use crate::{
     asset_tracking::LoadResource,
@@ -18,14 +21,76 @@ use crate::{
     AppSet,
 };
 
+#[derive(Component)]
+// Player with velocity
+struct JPlayer(pub f32);
+
 pub(super) fn plugin(app: &mut App) {
     app.register_type::<Player>();
     app.load_resource::<PlayerAssets>();
+
+    app.add_plugins(VirtualJoystickPlugin::<String>::default());
+    // Record directional input as movement controls.
+    app.add_systems(
+        Startup,
+        create_joystick_scene,
+    );
 
     // Record directional input as movement controls.
     app.add_systems(
         Update,
         record_player_directional_input.in_set(AppSet::RecordInput),
+    );
+
+    // Record directional input as movement controls.
+    app.add_systems(
+        Update,
+        update_joystick,
+    );
+}
+
+fn create_joystick_scene(mut cmd: Commands, asset_server: Res<AssetServer>) {
+    cmd.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(0., 0., 5.0),
+        ..default()
+    });
+    // Fake Player
+    cmd.spawn(SpriteBundle {
+        transform: Transform {
+            translation: Vec3::new(0., 0., 0.),
+            ..default()
+        },
+        texture: asset_server.load("Knob.png"),
+        sprite: Sprite {
+            color: Color::srgb(0.5, 0.0, 0.5), //Purple
+            custom_size: Some(Vec2::new(50., 50.)),
+            ..default()
+        },
+        ..default()
+    })
+    .insert(JPlayer(50.));
+
+    // Spawn Virtual Joystick at horizontal center using helper function
+    create_joystick(
+        &mut cmd,
+        "UniqueJoystick".to_string(),
+        asset_server.load("Knob.png"),
+        asset_server.load("Outline.png"),
+        None,
+        None,
+        None,
+        Vec2::new(75., 75.),
+        Vec2::new(150., 150.),
+        Style {
+            width: Val::Percent(100.),
+            height: Val::Percent(100.),
+            position_type: PositionType::Absolute,
+            left: Val::Percent(0.),
+            bottom: Val::Percent(0.),
+            ..default()
+        },
+        (JoystickInvisible, JoystickFloating),
+        NoAction,
     );
 }
 
@@ -112,6 +177,40 @@ fn record_player_directional_input(
         controller.intent = intent;
     }
 }
+
+
+fn update_joystick(
+    mut joystick: EventReader<VirtualJoystickEvent<String>>,
+    mut player: Query<(&mut Transform, &JPlayer)>,
+    time_step: Res<Time>,
+    mut controller_query: Query<&mut MovementController, With<Player>>
+) {
+    let (mut player, player_data) = player.single_mut();
+
+    let mut intent = Vec2::ZERO;
+
+    for j in joystick.read() {
+        let Vec2 { x, y } = j.axis();
+        
+        // controls the ball
+        player.translation.x += x * player_data.0 * time_step.delta_seconds();
+        player.translation.y += y * player_data.0 * time_step.delta_seconds();
+
+        println!("x: {}", x);
+        println!("y: {}", y);
+
+        // should be controlling the duck...
+        intent.x = *x;
+        intent.y = *y;
+        
+        // Apply movement intent to controllers.
+        for mut controller in &mut controller_query {
+            controller.intent = intent;
+        }
+    }
+
+}
+
 
 #[derive(Resource, Asset, Reflect, Clone)]
 pub struct PlayerAssets {
